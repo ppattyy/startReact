afb60a57d83109e5dcb7853046fab57998a8e5bc
ppattyy <galla355@msu.edu>
2025-03-17 22:22:57 +0000

    added spin

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import sharkPic from '/sharkpicNoBG.png'
 import './App.css'
 
 function App() {
@@ -14,6 +15,9 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://funnysharks.neocities.org/" target="_blank">
+          <img src={sharkPic} className="logo shark" alt="shark logo" />
         </a>
       </div>
       <h1>Vite + React + Patty</h1>
