e50b089802afe3b8133810d3f8b0edf1af748869
ppattyy <galla355@msu.edu>
2025-03-30 14:36:54 -0400

    10.5 lab

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,46 @@ import viteLogo from '/vite.svg'
 import sharkPic from '/sharkpicNoBG.png'
 import './App.css'
 
+function SharkButton() {
+  return <button>Shark</button>
+}
+
+function Header() {
+  return <h1>Here is my 10.5 lab! Just adding shark related components</h1>
+}
+
+function Footer() {
+  return <h1>Thank You! Have a good day.</h1>
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
+  function SharkList() {
+    const sharks = [
+      { id: 1, name: "Great White Shark", ocean: "All of them" },
+      { id: 2, name: "Bull Shark", ocean: "Atlantic, Pacific, Indian" },
+      { id: 3, name: "Tiger Shark", ocean: "Atlantic, Pacific, Indian" },
+      { id: 4, name: "Whale Shark", ocean: "Pacific, Indian" },
+    ];
+
+    return (
+      <div>
+        <h2>Shark Species</h2>
+        <ul>
+          {sharks.map((shark) => (
+            <li key={shark.id}>
+              {shark.name} - Lives in the {shark.ocean}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
   return (
     <>
+      <Header />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,6 +64,9 @@ function App() {
          Testing editing the page
         </p>
       </div>
+      <SharkButton />
+      <SharkList />
+      <Footer />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
